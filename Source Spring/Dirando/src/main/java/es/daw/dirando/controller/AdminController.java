161814f28af5b0433caeafc9c123238a10d6fcb8
danijimeno <dani.jimeno@hotmail.com>
2017-03-13 20:31:59 +0100

    Subida de imagenes en publicidad admin

--- Source Spring/Dirando/src/main/java/es/daw/dirando/controller/AdminController.java
+++ Source Spring/Dirando/src/main/java/es/daw/dirando/controller/AdminController.java
@@ -1,6 +1,7 @@
 package es.daw.dirando.controller;
 
 
+import java.io.File;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,15 +10,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import es.daw.dirando.repository.CategoriaRepository;
 import es.daw.dirando.repository.ProductoRepository;
+import es.daw.dirando.repository.PublicidadRepository;
 import es.daw.dirando.repository.UsuarioRepository;
 import es.daw.dirando.model.Categoria;
 import es.daw.dirando.model.Producto;
+import es.daw.dirando.model.Publicidad;
 
 @Controller
 public class AdminController {
+	
+	private static final String FILES_FOLDER = "src\\main\\webapp\\img";
+	private int i;
 
 	@Autowired
 	private ProductoRepository productoRepository;
@@ -27,6 +34,9 @@ public class AdminController {
 	
 	@Autowired
 	private UsuarioRepository userRepository;
+	
+	@Autowired
+	private PublicidadRepository publicidadRepository;
 
 
 	@RequestMapping("/admin/addProduct")
@@ -106,5 +116,50 @@ public class AdminController {
 		return "adminUsers";
 	}
    	
+    @RequestMapping("/admin/advertising")
+	public String advertising() {
+		return "adminAdvertising";
+	}
+    
+    @RequestMapping("/adming/advertising/upload")
+	public String uploadAdvertising(Model model, @RequestParam("imagen") MultipartFile imagen) {
+    	String fileName = i + ".jpg";
+		i++;
+		
+		if (!imagen.isEmpty()) {
+			try {
+
+				File filesFolder = new File(FILES_FOLDER);
+				if (!filesFolder.exists()) {
+					filesFolder.mkdirs();
+				}
+
+				File uploadedFile = new File(filesFolder.getAbsolutePath(), fileName);
+				imagen.transferTo(uploadedFile);
+				System.out.println(filesFolder.getAbsolutePath());
+				fileName="img/"+fileName;
+				Publicidad p = new Publicidad("",fileName);
+				publicidadRepository.save(p);
+				
+				//model.addAttribute("imageTitles", imageTitles);
+				
+				return "adminAdvertising";
+
+			} catch (Exception e) {
+				
+				model.addAttribute("fileName",fileName);
+				model.addAttribute("error",
+						e.getClass().getName() + ":" + e.getMessage());
+				
+				return "adminAdvertising";
+			}
+		} else {
+			
+			model.addAttribute("error",	"The file is empty");
+			
+			return "adminAdvertising";
+		}
+   
+	}
 	
 }
