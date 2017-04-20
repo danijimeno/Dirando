e49b48acdc5f73e3c0d1d3b100fb272f9989d862
rperezll <robertoprz898@gmail.com>
2017-04-20 10:09:45 +0200

    Uso de objeto para descarga de Carousel

--- angularDirando/dirandoApp/src/app/indexbody/indexbody.component.ts
+++ angularDirando/dirandoApp/src/app/indexbody/indexbody.component.ts
@@ -1,36 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 
-
 @Component({
   selector: 'app-indexbody',
   templateUrl: './indexbody.component.html',
   styleUrls: ['./indexbody.component.css']
 })
+
 export class IndexbodyComponent{
-  
-  private imagen: string[]= [];
+  private imagen: Object[]= [];
 
   constructor(private http: Http) {
     console.log("Llega");
     this.loadCarousel();
    }
+
   loadCarousel() {
     console.log("Llega2");
       let url = "https://localhost:8443/rest/carrusel";
       this.http.get(url).subscribe(
         response => {
           let data = response.json();
+          console.log(data);
           for (var i = 0; i < data.length; i++) {
-            let img = data[i].imagen;
-            let nom = data[i].nomPublicidad;
-            //this.imagen.push({"src":"img","alt":"nom"});
+            this.imagen.push({"name":data[i].nomPublicidad,"image":data[i].imagen}); 
           }
-      console.log(this.imagen);
+        console.log(this.imagen);
         },
         error => console.error(error)
       );
    }
-
-
 }
