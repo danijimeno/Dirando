dfe0c7c471846260341ec7f870829bfd09d67475
rperezll <robertoprz898@gmail.com>
2017-05-14 17:33:01 +0200

    Corrección docker-compose + limpieza repositorio

--- Dirando-Angular/src/main/java/es/daw/dirando/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package es.daw.dirando.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import es.daw.dirando.model.Comment;
-
-
-public interface CommentRepository extends JpaRepository<Comment, Long>{
-}
